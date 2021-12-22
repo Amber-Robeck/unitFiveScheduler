f40a62f1f608f2fc7b4912b935ec79d616961c43
Amber Robeck <arr5533@gmail.com>
2021-12-22 17:42:45 -0600

    added methods to script.js to return only number from id and parseInt to number from string

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function workDayHours() {
     console.log(hour)//logs as 17 @ 5/Military time 24 hour maybe add class to time blocks of 13++
 }
 workDayHours();//REMEMBER TO CALL FUNCTIONS
+
+
+//Need to get number out of string ex..hour8 to equal 8
+// function getNumFromStr () {
+//     var tName = 'arun_4874_541';
+//     alert(tName.match(/\d+/g));
+// }
+
+blockTime = "hour8".match(/\d+/)[0] //converting to just number [0] returns string without [0] returns object
+console.log(parseInt(blockTime));
+console.log(typeof blockTime);
+console.log(typeof parseInt(blockTime));
