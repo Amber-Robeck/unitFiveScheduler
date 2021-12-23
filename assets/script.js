0b08f0949e80e901443f098a266fb8d3d03766da
Amber Robeck <arr5533@gmail.com>
2021-12-23 14:22:22 -0600

    removed unused code and removed un-needed comments

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-
-
 //displays the date and time and updates by the second from moment.js
 var dateTime = null,
     todayDate = null;
 var keepTime = function () {
-    todayDate = moment(new Date())
+    todayDate = moment(new Date())//Date method returns date as string
     dateTime.html(todayDate.format('dddd, MMMM Do YYYY, h:mm:ss a'));
 };
 $(document).ready(function () {
@@ -12,47 +10,18 @@ $(document).ready(function () {
     keepTime();
     setInterval(keepTime, 1000);
 });//end of time/keepTime functions
-var testHour = moment().add(12, 'hours').hour();
-console.log(testHour);
+
+//This was created to test logic at all hours of the day adding 12 hours to current time
+//Simply change number after add and then replace hour with testHour 
+// var testHour = moment().add(12, 'hours').hour();
+// console.log(testHour);
 
 var hour;
 function workDayHours() {
     hour = moment().hour();
-    console.log(hour)//logs as 17 @ 5/Military time 24 hour maybe add class to time blocks of 13++
+    console.log(hour)//hour logs in 24 hours 1pm == 13
 }
 workDayHours();//REMEMBER TO CALL FUNCTIONS
-
-//hour=blockTime then add class of present
-//hour>blockTime then add class of past
-//hour<blockTIme then add class of future
-
-
-//Need to get number out of string ex..hour8 to equal 8
-// function getNumFromStr () {
-//     var tName = 'arun_4874_541';
-//     alert(tName.match(/\d+/g));
-// }
-
-// blockTime = "hour8".match(/\d+/)[0] //converting to just number [0] returns string without [0] returns object
-// console.log(parseInt(blockTime));
-// console.log(typeof blockTime);
-// console.log(typeof parseInt(blockTime));
-
-// if (hour < 19) {
-//     alert("it is very late")
-// }
-
-// $('#container').children('#id').each(function () {
-//     alert(this.value); // "this" is the current element in the loop
-// });
-// var hour8 = $('#hour8');<-----I have created an infinite loop lol
-
-// $(".time-block").each(function () {
-//     var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-//     console.log(blockHour)
-// })
-//these two functions do the same exact thing
-
 
 //for each time-block check to see if it is < > === to hour
 $(".time-block").each(function () {
@@ -69,37 +38,6 @@ $(".time-block").each(function () {
         $(this).addClass("future")
     }
 })
-
-//
-// $(".time-block").each(function () {
-//     var blockHour = parseInt($(this).attr("id").match(/\d+/)[0]);
-//     console.log(blockHour)
-//     if (blockHour < testHour) {
-//         $(this).addClass("past")
-//     }
-//     else if (blockHour === testHour) {
-//         $(this).addClass("present")
-//     }
-//     else {
-//         $(this).addClass("future")
-//     }
-// })
-
-//when save button on click save textarea, for each function .description.val
-
-// $(".saveBtn").on("click", function () {
-//     var times = [];
-//     console.log("working")
-//     localStorage.setItem("user", JSON.stringify());
-// })
-
-// $(".saveBtn").each(function() {
-// $(".saveBtn").on("click", function () {
-
-//     var times = [];
-//     console.log("working")
-//     localStorage.setItem("user", JSON.stringify());
-// })
 
 //when clicking on save button, saving input along with time block number to local storage
 $(".saveBtn").on("click", function () {
@@ -127,19 +65,8 @@ if (localStorage.val == "") {
     $("#hour17 .description").val(localStorage.getItem("hour17"));
 }
 
-// var button = document.createElement("button")
-// button.innerHTML = "Reset all fields"
-// document.body.appendChild(button);
-// button.addEventListener("click", function () {
-//     localStorage.clear()
-//     $(this).siblings(".description").val();
-// })
 
-// $("#myButton").click(function () {
-//     var test = $('<button>Test</button>').click(function () {
-//         alert('hi');
-//     })
-
+//Creating reset button and appending to container
 $('<button/>', {
     text: 'Reset all fields',
     class: 'resetBtn'
